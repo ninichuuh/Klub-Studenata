62ae338b433acf0a9f4b9b9b4a610fe5a64d55b0
peroautogit <peroautogit@gmail.com>
2022-10-18 12:59:13 +0200

    Loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,16 +2,58 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <svg width="200px" height="200px" viewBox="-4 -1 38 28">
-        <polygon
-          fill="transparent"
-          stroke="#FFFF"
-          stroke-width="2"
-          points="15,30 30,0 0,0"
-        ></polygon>
-      </svg>
-    </div>
+    <svg viewBox="0 0 340 333">
+      <path
+        d="M-14.977094,-12.334077L14.977094,12.334077"
+        class="path"
+        transform="translate(76.700344 93.087131)"
+        fill="none"
+        stroke="whitesmoke"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="bevel"
+      />
+      <path
+        d="M14.977094,-18.324915l-29.954188,36.64983"
+        class="path"
+        transform=" matrix(1 0 0 1.057692 76.700344 124.803324)"
+        fill="none"
+        stroke="whitesmoke"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="bevel"
+      />
+      <path
+        d="M-63.256196,-43.697874L63.256196,43.697874"
+        class="path"
+        transform=" translate(124.979446 187.883319)"
+        fill="none"
+        stroke="whitesmoke"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="bevel"
+      />
+      <path
+        d="M-0.352403,81.581111l.704806-163.162222"
+        class="path"
+        transform="matrix(1 0 0 1.000001 188.588045 150)"
+        fill="none"
+        stroke="whitesmoke"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="bevel"
+      />
+      <path
+        d="M-25.372959,-16.386703L25.372959,16.386702"
+        class="path"
+        transform="matrix(.972222 0 0 0.999992 213.608601 84.805674)"
+        fill="none"
+        stroke="whitesmoke"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="bevel"
+      />
+    </svg>
   );
 };
 
